b1937c1ccc528c8eb3c889cf92c2f6f0f5b5d8ad
sora-zhang-42 <soraraso42@gmail.com>
2024-10-05 21:11:57 -0400

    change style at .cls

---
+++ R/Untitled.R
@@ -0,0 +1,46 @@
+projects <- tribble(
+  ~area, ~accomplishment, ~year, ~where, ~detail,
+  "MuleSoft Platform Implementation", "Covered California", 2024, "Covered California IT Data Team", "Implemented the MuleSoft Anypoint Platform for Covered California involving infrastructure and developer tooling such as CICD and GitHub based automation.",
+  "Lean Six Sigma Green Belt --- Reception Center Intake Process", "California Correctional Health Care Services", 2019, "Region 1 Quality Management", "Completed a Lean Six Sigma Green Belt project on the reception center medical screening process at San Quentin State Prison improving compliance from 2% and a baseline capability aggregate of 29.96% compliance to 84% during the transition phase of the project through application of the lean 6 methodologies and tools. Scope of project affects 6 total state prisons with 3 more to be positively impacted by state wide changes resulting from this project",
+  "Universal Data Scrubber", "California Correctional Health Care Services", 2018, "Region 1 Quality Management", "Creation of a Universal Data Scrubbing tool sent statewide to take most department SQL Report Server exports and turn them into raw data sets for Excel Analytics for end user staff"
+)
+
+certifications <- tribble(
+  ~area, ~accomplishment, ~year, ~where, ~detail,
+  "Programming Language", "C# Foundation", 2024, "", "Credential ID X5Q95GN4",
+)
+
+skills <- tribble(
+  ~area, ~skills,
+  "Programming Languages", "BASH, Batch, CCL, DAX, JavaScript, Python, R, T-SQL, PL-SQL, VBA",
+  "Markup Languages", "CSS, HTML, LaTeX, Markdown, RMarkdown, SASS",
+  "Data Interchange Formats", "CSV, JSON, XML, Feather",
+  "Version Control", "Git",
+  "Text Editors", "Discern Visual Developer, RStudio, SQL Server Management Studio, VIM, Visual Studio, Visual Studio Code",
+  "Microsoft Office", "PowerPoint, Excel, Word, Outlook, OneNote,"
+)
+
+honors <- tribble(
+  ~area, ~accomplishment, ~year, ~where, ~detail,
+  "Web Developement and Internet Application", "Dean's List", 2024, "Algonquin College of Technology",NA,
+  
+)
+
+edu <- tribble(
+  ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
+  
+  "Diploma", 2007, 2011, "Galt High School", "Galt - California", "AP English Literature",
+  "Diploma", 2007, 2011, "Galt High School", "Galt - California", "AP US Government"
+)
+
+work <- tribble(
+  ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "Information Technology Specialist II", "Covered California", "August", 2023, "Present", NA, "Sacramento - California", "LANGUAGES USED --- T-SQL, Markdown, Python, YAML, HTML, Powershell, Bash, Batch, XML",
+  
+)
+
+works <- tribble(
+  ~area, ~accomplishment, ~year, ~where, ~detail,
+  "Runes", "Convert a string of text characters to Elder Futhark Runes ", "February 2020", "https://github.com/tallguyjenks/runes", "Author and Maintainer",
+  "Runes", "Convert a string of text characters to Elder Futhark Runes ", "February 2020", "https://github.com/tallguyjenks/runes", "Released to CRAN"
+)
